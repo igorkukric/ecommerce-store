d93406cc3a7ebbc61d5e35c8721f6547f998c684
Igor_Kukric <igorkukric@gmail.com>
2023-10-29 22:56:59 +0100

    final

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/ui/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
+
+export const relative = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("abd2c0cc-d305-4a27-bfcf-21339af3427a");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
