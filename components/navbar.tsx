32a8c3fa299f4c2c796d929dc8f3137b659f20e2
Igor_Kukric <igorkukric@gmail.com>
2023-11-03 14:43:31 +0100

    fix responsivity

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,24 +5,27 @@ import MainNav from "@/components/main-nav";
 import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
 
-export const relative = 0
+export const relative = 0;
 
 const Navbar = async () => {
-    const categories = await getCategories()
+  const categories = await getCategories();
 
-    return ( 
-        <div className="border-b"> 
-            <Container>
-                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center ">
-                <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-                  <p className="font-bold lg:text-xl">New Line Shop</p>  
-                </Link>
-                <MainNav data={categories} />
-                <NavbarActions />
-                </div>
-            </Container>
+  return (
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold lg:text-xl">
+              <span className="hidden sm:inline">New Line Shop</span>
+              <span className="sm:hidden">NLS</span>
+            </p>
+          </Link>
+          <MainNav data={categories} />
+          <NavbarActions />
         </div>
-     );
-}
- 
+      </Container>
+    </div>
+  );
+};
+
 export default Navbar;
