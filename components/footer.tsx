999999ed6a16e015406ff94918afb4ebe31362e6
Igor_Kukric <igorkukric@gmail.com>
2023-11-04 15:47:59 +0100

    change next.config add footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,76 @@
-const Footer = () => {
-  return (
-    <footer className="bg-white border-t">
-      <div className="mx-auto py-10">
-        <p className="text-center text-xs text-black">
-          &copy; 2023 Online Shop, Sva Prava Zadržana.
-        </p>
-      </div>
-    </footer>
-  )
+import Link from "next/link";
+import Container from "@/components/ui/container";
+
+import { footerLinks } from "@/constant";
+
+type ColumnProps = {
+  title: string;
+  links: Array<string>;
 };
+
+const FooterColumn = ({ title, links }: ColumnProps) => (
+  <div className="footer_column max-lg:ml-5">
+    <h4 className="font-semibold">{title}</h4>
+    <ul className="flex flex-col gap-2 font-normal text-gray-600">
+      {links.map((link) => (
+        <Link href="/" key={link}>
+          {link}
+        </Link>
+      ))}
+    </ul>
+  </div>
+);
+
+const Footer = () => (
+  <Container>
+    <section>
+      <div className="flex flex-col gap-12 w-full">
+        <div className="flex flex-wrap gap-12">
+          <FooterColumn
+            title={footerLinks[0].title}
+            links={footerLinks[0].links}
+          />
+
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[1].title}
+              links={footerLinks[1].links}
+            />
+            <FooterColumn
+              title={footerLinks[2].title}
+              links={footerLinks[2].links}
+            />
+          </div>
+
+          <FooterColumn
+            title={footerLinks[3].title}
+            links={footerLinks[3].links}
+          />
+
+          <div className="flex-1 flex flex-col gap-4">
+            <FooterColumn
+              title={footerLinks[4].title}
+              links={footerLinks[4].links}
+            />
+            {/* <FooterColumn
+              title={footerLinks[5].title}
+              links={footerLinks[5].links}
+            /> */}
+          </div>
+
+          <FooterColumn
+            title={footerLinks[6].title}
+            links={footerLinks[6].links}
+          />
+        </div>
+      </div>
+
+      <div className="flex justify-end">
+        <p>@ 2023 New Line Shop. Sva Prava Zadržana.</p>
+        
+      </div>
+    </section>
+  </Container>
+);
 
 export default Footer;
---
+++ constant/index.ts
@@ -0,0 +1,56 @@
+export const footerLinks = [
+    {
+        title: "BRENDOVI",
+        links: ["Grealy", "Quartz", "Geneva", "Naidu"],
+      },
+ 
+  {
+    title: "KOMPANIJA NEW LINE",
+    links: [
+      "O Kompaniji New Line Shop",
+      "Društveno odgovorno poslovanje",
+      "Posao u NLS",
+      "Zakupljujemo lokale",
+      "Company profile",
+    ],
+  },
+  {
+    title: "PRODAJA",
+    links: ["Akcije", "Novosti"],
+  },
+  {
+    title: "KUPOVINA",
+    links: [
+      "NLS loyalty kartica",
+      "Uslovi korišćenja",
+      "Politika privatnosti",
+      "Detajli ugovora o prodaji",
+      "Obaveštenje o pravima i obavezama potrošača",
+      "Reklamacije",
+      "Prodavnice",
+      "Osiguranje proizvoda",
+    ],
+  },
+  {
+    title: "POTREBNA VAM JE POMOĆ?",
+    links: [
+      "Kontakt",
+      "Kako kupovati na NLS",
+      "Česta pitanja",
+      "Šta su New Line bodovi",
+    ],
+  },
+  {
+    title: "Development assets",
+    links: [
+      "Code Marketplace",
+      "GitHub Marketplace",
+      "NPM Registry",
+      "Packagephobia",
+    ],
+  },
+  {
+    title: "PLAĆANJE I ISPORUKA",
+    links: ["Način plaćanja", "Isporuka", "TAX FREE i Ambasade"],
+  },
+];
