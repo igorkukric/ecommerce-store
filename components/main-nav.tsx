6b65f092007104e5a5a05fa75d6f03c41dd8f96a
Igor_Kukric <igorkukric@gmail.com>
2023-11-04 21:32:42 +0100

    add toggle menu

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -1,17 +1,27 @@
 "use client";
 
+import React, { useState } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-
 import { cn } from "@/lib/utils";
 import { Category } from "@/types";
+import { Menu } from "lucide-react";
 
 interface MainNavProps {
   data: Category[];
 }
 
 const MainNav: React.FC<MainNavProps> = ({ data }) => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const pathname = usePathname();
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  };
 
   const routes = data.map((route) => ({
     href: `/category/${route.id}`,
@@ -21,18 +31,52 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
 
   return (
     <nav className="mx-6 flex items-center space-x-4 lg:space-x-6">
-      {routes.map((route) => (
-        <Link
-          key={route.href}
-          href={route.href}
-          className={cn(
-            "text-sm font-medium transition-colors hover:text-black",
-            route.active ? "text-black" : "text-neutral-500"
-          )}
+      <button
+        className="md:hidden p-4 rounded-md focus:border-gray-400 focus:border"
+        onClick={toggleMobileMenu}
+      >
+        <Menu />
+      </button>
+      <ul
+        className={`md:flex md:space-x-6 hidden lg:flex space-x-4`}
+      >
+        {routes.map((route) => (
+          <li key={route.href}>
+            <Link
+              href={route.href}
+              className={cn(
+                "text-sm font-medium transition-colors hover:text-black",
+                route.active ? "text-black" : "text-neutral-500"
+              )}
+            >
+              {route.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      <div className="relative">
+        <ul
+          className={`absolute right-0 top-12 bg-white rounded-md shadow-lg flex flex-col space-y-2 ${
+            mobileMenuOpen ? "block" : "hidden"
+          }`}
+          onClick={closeMobileMenu}
+          style={{ zIndex: 2 }}
         >
-          {route.label}
-        </Link>
-      ))}
+          {routes.map((route) => (
+            <li key={route.href}>
+              <Link
+                href={route.href}
+                className={cn(
+                  "block py-2 px-4 text-lg font-medium transition-colors hover:text-black",
+                  route.active ? "text-black" : "text-neutral-500"
+                )}
+              >
+                {route.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
